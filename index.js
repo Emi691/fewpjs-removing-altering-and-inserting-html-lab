13783457ea303ede2344483d92e9aa297047c088
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-13 14:41:10 -0700

    commit tree 2529cd7949d428845efe8a1e1a4dcfbc7269f671

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector('main#main');
+main.remove();
+
+let header = document.createElement('h1');
+header.setAttribute('id','victory');
+header.innerHTML = 'Emiko is the champion';
+document.body.appendChild(header);
+let newHeader = document.querySelector('h1#victory');
